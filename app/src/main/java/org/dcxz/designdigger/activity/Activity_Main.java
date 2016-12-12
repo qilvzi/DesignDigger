43355943eb45cbcd7d0b9eb0b7ec0d602408ec03
邪恶的黑手 <2325690622@qq.com>
2016-12-13 00:14:06 +0800

    更新继承关系;

---
+++ app/src/main/java/org/dcxz/designdigger/activity/Activity_Main.java
@@ -0,0 +1,41 @@
+package org.dcxz.designdigger.activity;
+
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.dcxz.designdigger.R;
+import org.dcxz.designdigger.framework.Framework_Activity;
+
+public class Activity_Main extends Framework_Activity {
+
+    @Override
+    protected int setContentViewImp() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initAdapter() {
+
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    public void handleMessageImp(Message msg) {
+
+    }
+}
